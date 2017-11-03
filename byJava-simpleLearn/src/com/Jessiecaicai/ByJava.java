bef2c01492a3d94bed470c30be08345e706bf6dd
Jessiecaicai <365444091@qq.com>
2017-11-03 23:06:06 +0800

    simple file writed by Java

---
+++ byJava-simpleLearn/src/com/Jessiecaicai/ByJava.java
@@ -0,0 +1,45 @@
+package com.Jessiecaicai;
+//import java.util.Calendar;
+///**
+// * @Author: Jessiecaicai
+// * @Description:获得学号输出对应年级
+// * @Date: Created in 22:34 2017/11/3
+// * @Modified By:
+// */
+//
+//public class TimeAndSno {
+//    //Calendar c = Calendar.getInstance();
+//    //int year = c.get(Calendar.YEAR);
+//
+//    public static void main(String args[]){
+//        //TimeAndSno timeAndSno=new TimeAndSno();
+//        //timeAndSno.
+//        Calendar c = Calendar.getInstance();
+//        int year = c.get(Calendar.YEAR);
+//        int month = c.get(Calendar.MONTH);
+//        int sno=2015;
+//        System.out.println(month+1);
+//        int result=0;
+//        if(month>=8){
+//            result=year-sno+1;
+//        }
+//        else{
+//            result=year-sno;
+//        }
+//
+//        while(result==1){
+//            System.out.println("大一");
+//        }
+//        while(result==2){
+//            System.out.println("大二");
+//        }
+//        while(result==3){
+//            System.out.println("大三");
+//            break;
+//        }
+//        while(result==4){
+//            System.out.println("大四");
+//        }
+//    }
+//}
+
